d00b350f63b89d36d0ef47d21534f104c3d95119
murilonicemento <murilo_nascimento22@outlook.com>
2023-03-21 19:43:23 -0300

    style: Finalização da estilização no layout Mobile

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -2,5 +2,35 @@ import React from "react";
 import "./Form.css";
 
 export default function Form() {
-  return <div></div>;
+  return (
+    <form action="">
+      <label htmlFor="cardholder-name">Cardholder Name</label>
+      <input
+        type="text"
+        id="cardholder-name"
+        placeholder="e.g Jane Appleseed"
+      />
+      <label htmlFor="card-number">Card Number</label>
+      <input
+        type="number"
+        id="card-number"
+        placeholder="e.g 1234 5678 9123 0000"
+      />
+      <div>
+        <div id="date-information">
+          <div>
+            <label htmlFor="month">Exp. Date (MM/YY)</label>
+            <input type="number" id="month" placeholder="MM" />
+            <input type="number" id="year" placeholder="YY" />
+          </div>
+        </div>
+        <span id="cod-information">
+          <label htmlFor="cvc">cvc</label>
+          <input type="number" id="cvc" placeholder="e.g 123" />
+        </span>
+      </div>
+
+      <button>Confirm</button>
+    </form>
+  );
 }
