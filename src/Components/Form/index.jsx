9ce5b365616c3b185bde5808fcc3c89714417c82
murilonicemento <murilo_nascimento22@outlook.com>
2023-03-22 19:47:04 -0300

    Inclusão de verificação dos inputs

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./Form.css";
 
-export default function Form() {
+export default function Form({ handleChange, handleSubmit }) {
   return (
     <form action="">
       <label htmlFor="cardholder-name">Cardholder Name</label>
@@ -9,28 +10,45 @@ export default function Form() {
         type="text"
         id="cardholder-name"
         placeholder="e.g Jane Appleseed"
+        onChange={handleChange}
       />
       <label htmlFor="card-number">Card Number</label>
       <input
         type="number"
         id="card-number"
         placeholder="e.g 1234 5678 9123 0000"
+        onChange={handleChange}
       />
       <div>
         <div id="date-information">
           <div>
             <label htmlFor="month">Exp. Date (MM/YY)</label>
-            <input type="number" id="month" placeholder="MM" />
-            <input type="number" id="year" placeholder="YY" />
+            <input
+              type="number"
+              id="month"
+              placeholder="MM"
+              onChange={handleChange}
+            />
+            <input
+              type="number"
+              id="year"
+              placeholder="YY"
+              onChange={handleChange}
+            />
           </div>
         </div>
         <span id="cod-information">
           <label htmlFor="cvc">cvc</label>
-          <input type="number" id="cvc" placeholder="e.g 123" />
+          <input
+            type="number"
+            id="cvc"
+            placeholder="e.g 123"
+            onChange={handleChange}
+          />
         </span>
       </div>
 
-      <button>Confirm</button>
+      <button onSubmit={handleSubmit}>Confirm</button>
     </form>
   );
 }
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -5,6 +5,30 @@ import backCardImg from "../img/bg-card-back.png";
 import "./Main.css";
 
 export default class Main extends Component {
+  handleChange = (event) => {
+    event.preventDefault();
+
+    const cardholder = document.getElementById("cardholder-name");
+    const cardNumber = document.getElementById("card-number");
+    const month = document.getElementById("month");
+    const year = document.getElementById("year");
+    const cvc = document.getElementById("cvc");
+
+    if (/e/i.test(cardNumber.value)) {
+      cardNumber.style.border = "1px solid red";
+    }
+
+    if (
+      cardholder.value == "" ||
+      cardNumber.value == "" ||
+      month.value == "" ||
+      year.value == "" ||
+      cvc.value == ""
+    ) {
+      console.log("ok");
+    }
+  };
+
   render() {
     return (
       <main>
@@ -12,7 +36,10 @@ export default class Main extends Component {
           <img src={backCardImg} alt="Card Back" />
           <img src={frontCardImg} alt="Card Front" />
         </section>
-        <Form />
+        <Form
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
       </main>
     );
   }
